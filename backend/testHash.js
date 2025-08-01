c28caf386380c1f620f6c9d4b830c62ae752567e
alancabj <alan_asv@hotmail.com>
2025-07-31 21:36:55 -0300

    Eliminar backend del repositorio frontend

--- backend/testHash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-(async () => {
-  const password = '123';
-  const hash = '$2b$10$41879WG2U4rCzpY3BkeDk.3O3pNwK8zgXguU6azLU9uBlQB1wbzp.'; // hash que tienes guardado
-
-  const match = await bcrypt.compare(password, hash);
-  console.log('¿Coincide la contraseña?', match);
-})();
